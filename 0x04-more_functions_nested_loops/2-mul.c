e4e681690f9fc2a236645feb6891863c4ee2f7ce
Tafara N <c.tafaranyamhunga@gmail.com>
2023-10-26 08:51:52 +0200

    A function that checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mul - function checks for a digit (0 through).
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul =a * b;
+	
+	return (mul);
+}
