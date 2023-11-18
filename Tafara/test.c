b1388e927ca4b61ee165a44453aafb65b6703dbb
Tafara N <c.tafaranyamhunga@gmail.com>
2023-11-18 15:51:58 +0200

    Testing

---
+++ Tafara/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Success 0
+ *
+ * Description: Testing git push
+ */
+
+int main(void)
+{
+	printf("My name is Tafara");
+
+	return (0);
+}
