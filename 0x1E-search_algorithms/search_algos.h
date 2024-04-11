4e69737ae01c2d18168cfbd739e75927fad05de1
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-11 23:07:54 +0200

    Function prototypes.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -51,5 +51,8 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int recursive_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 
 #endif  /* _SORTING_ALGOS_H_ */
