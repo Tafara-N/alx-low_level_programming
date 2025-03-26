2951225ffdb1ef86fcea14bb51d7807c7a45dcab
Tafara-N <c.tafaranyamhunga@gmail.com>
2025-03-26 15:38:14 +0200

    Program prints exactly: and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,9 +12,8 @@
 
 int main(void)
 {
-	char *ptr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, ptr, strlen(ptr));
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
